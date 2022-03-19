7f66ac9ed4912d6aaf138ab6767e44cc0bae7f35
luke-davison <luke.warmwater6@gmail.com>
2022-03-19 17:09:19 +1300

    Added touch support

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,9 @@
 import { observer } from 'mobx-react-lite';
 import { useState } from 'react';
+import { isMobile } from 'react-device-detect';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 
 import { GameContext } from '../stores/GameContext';
 import { GameStore } from '../stores/GameStore';
@@ -9,10 +11,12 @@ import { GameArea } from './GameArea';
 
 export const Game: React.FC = observer(() => {
   const [gameStore] = useState<GameStore>(new GameStore())
+  
+  const backend = isMobile ? TouchBackend : HTML5Backend
 
   return (
     <GameContext.Provider value={gameStore}>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={backend}>
         <GameArea/>
       </DndProvider>
     </GameContext.Provider>
