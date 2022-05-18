0b6e317e62634428407f4b76455f13e669b2cb5b
Luke Davison <luke.davison@bpsoftware.net>
2022-05-18 16:53:06 +1200

    Initial graph

---
+++ src/components/Graph.tsx
@@ -0,0 +1,56 @@
+import "./Graph.css"
+
+import { observer } from "mobx-react-lite";
+import { FunctionComponent } from "react";
+import { IGameStats } from "../shared";
+
+interface IGraphProps {
+  stats: IGameStats;
+}
+
+export const Graph: FunctionComponent<IGraphProps> = observer(props => {
+  const { results } = props.stats
+
+  const xMin: number = results.reduce((min: number | undefined, result) => {
+    return (!min || result[0] < min) ? result[0] : min
+  }, undefined) || 0
+  const xMax: number = results.reduce((max: number, result) => {
+    return result[0] > max ? result[0] : max
+  }, 0)
+  const yMin: number = 0
+  const yMax: number = results.reduce((max: number, result) => {
+    return result[1] > max ? result[1] : max
+  }, 0)
+  const yRange = yMax - yMin
+
+  const xAxis: number[] = [...new Array(xMax - xMin + 1)].map((_, index) => index + xMin)
+
+  return (
+    <div className="graph">
+      <div className="graph-inner">
+        { xAxis.map(xAxe => {
+          const result = results.find(result => result[0] === xAxe)
+          const value = result ? result[1] : 0
+
+          const barHeight = (value - yMin)  / yRange
+          console.log(value, yRange)
+
+          return (
+            <div key={xAxe} className="x-column">
+              <div className="x-column-inner">
+                { value > 0 && (
+                  <div className="graph-bar" style={{height: (barHeight * 100).toFixed(0) + "%" }} />
+                )}
+              </div>
+              <div className="x-column-axe">
+                <span>
+                  { xAxe }
+                </span>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+})
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,6 +5,13 @@ import React, { useContext } from 'react';
 
 import { AppContext } from '../stores/AppContext';
 import { Game } from './Game';
+import { Graph } from './Graph';
+import { IGameStats } from '../shared';
+
+const dummyStats: IGameStats = {
+  date: "2022-05-18",
+  results: [[10, 1], [15, 3], [16, 4], [18, 2], [19, 5], [20, 7], [21, 11], [22, 5], [24, 16], [25, 21], [26, 28], [27, 10], [28, 21], [29, 60], [30, 72], [31, 130]]
+}
 
 export const Menu: React.FC = observer(() => {
   const { startCampaignGame, isPlayingCampaignGame, isPlayingDailyGame, startDailyGame, startTutorialGame, isPlayingTutorialGame, gameId } = useContext(AppContext)
@@ -34,6 +41,7 @@ export const Menu: React.FC = observer(() => {
       <div>
         <button onClick={startTutorialGame}>Tutorial</button>
       </div>
+      <Graph stats={dummyStats}/>
     </div>
   );
 })
